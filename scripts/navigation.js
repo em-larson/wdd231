afba5c49e4cf264136e16822fe9506650b5d49af
em-larson <elarson1@byupathways.edu>
2025-09-06 00:00:44 -0600

    Created homepage for WDD231

---
+++ scripts/navigation.js
@@ -0,0 +1,22 @@
+
+//responsive menu code//
+const hamButton = document.querySelector("#menu");
+const navigation = document.querySelector("#mainNav");
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
+
+//footer code//
+
+const year = new Date().getFullYear();
+const month = new Date().getMonth() + 1;
+const day = new Date().getDate();
+
+document.getElementById("copyright").innerHTML = `&copy ${year} | Emily Larson | Utah, USA`;
+document.getElementById('modified').textContent = `Date Modified: ${month}/${day}/${year}`;
+
+
+
+
