c4a304882d8500347527d3de09d34fd305388086
em-larson <elarson1@byupathways.edu>
2025-10-05 11:36:35 -0600

    finished js to create cards

---
+++ chamber/scripts/discover.js
@@ -0,0 +1,61 @@
+const discovercards = document.querySelector('#discoverCards')
+
+//responsive menu code//
+const hamButton = document.querySelector("#menu");
+const navigation = document.querySelector("#mainNav");
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
+
+//footer code//
+
+const year = new Date().getFullYear();
+const month = new Date().getMonth() + 1;
+const day = new Date().getDate();
+
+document.getElementById("copyright").innerHTML = `&copy ${year} | Emily Larson | Utah, USA`;
+document.getElementById('modified').textContent = `Date Modified: ${month}/${day}/${year}`;
+
+//create discover cards//
+
+const fetchDiscoverData = async () => {
+    try {
+        const response = await fetch("discover.json");
+        const data = await response.json();
+        displayDiscoverCard(data.discover);
+    } catch (error) {
+        let errormessage = document.createElement("h3");
+        errormessage.innerHTML = "Information could not be loaded.",
+        discovercards.appendChild(errormessage);
+    }
+}
+
+fetchDiscoverData();
+
+const displayDiscoverCard = (discover) => {
+    discover.forEach((item) => {
+        let card = document.createElement("section");
+        let name = document.createElement("h3");
+        let pic = document.createElement("img");
+        let address= document.createElement("p");
+        let description = document.createElement("p");
+
+        name.innerHTML = `${item.name}`
+
+        pic.setAttribute('src', item.picture);
+        pic.setAttribute('alt', item.name);
+        pic.setAttribute('loading', 'lazy');
+
+        address.innerHTML = `${item.address}`;
+        description.innerHTML = `${item.description}`;
+
+        card.appendChild(name);
+        card.appendChild(pic);
+        card.appendChild(address);
+        card.appendChild(description);
+
+        discovercards.appendChild(card);
+    });
+}
