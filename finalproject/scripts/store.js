e8c66ad74f3634848fb7b32e5e03b2e026a33101
em-larson <elarson1@byupathways.edu>
2025-10-11 17:13:05 -0600

    got the scroll to work!

--- finalproject/scripts/store.js
+++ finalproject/scripts/store.js
@@ -3,18 +3,22 @@
 const scrollAmount = 300;
 
 document.addEventListener("DOMContentLoaded", function () {
-    const galleryContainer = document.querySelector('.gallery');
-    const leftBtn = document.querySelector('.scrollLeft');
-    const rightBtn = document.querySelector('.scrollRight');
+    const galleryContainer = document.querySelectorAll('.galleryContainer');
 
-    leftBtn.addEventListener('click', () => {
-        galleryContainer.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
-    })
+    galleryContainer.forEach(container => {
 
-    rightBtn.addEventListener('click', () => {
-        galleryContainer.scrollBy({ left: scrollAmount, behavior: 'smooth' });
-        console.log('its working');
-    })
+        const gallery = container.querySelector('.gallery');
+        const leftBtn = container.querySelector('.scrollLeft');
+        const rightBtn = container.querySelector('.scrollRight');
 
+        leftBtn.addEventListener('click', () => {
+            gallery.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
+        });
+
+        rightBtn.addEventListener('click', () => {
+            gallery.scrollBy({ left: scrollAmount, behavior: 'smooth' });
+            console.log('its working');
+        });
+    });
 });
 
